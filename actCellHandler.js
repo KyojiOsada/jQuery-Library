03a74eba2676f51c0ba72addd89b4b40f5e7d443
Kyoji Osada <39667755+KyojiOsada@users.noreply.github.com>
2018-10-25 11:00:56 +0900

    Update and rename cell_handler.js to actCellHandler.js

--- actCellHandler.js
+++ actCellHandler.js
@@ -1,3 +1,12 @@
+/*!
+ * actCellHandler v1.0.0
+ *
+ * Require:     jquery
+ * Copyright:   2016, AiR&D Inc. (http://a-i-r-d.co.jp/)
+ * Author:      Kyoji Osada
+ * Licensed:    Under the Apache License 2.0
+ */
+
 var Act =
 {
 	//* Objective Property Init
